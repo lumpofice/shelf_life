036150f9f30829f1469d0ca8f639912700298201
lumpofice <jparker007@gmail.com>
2026-01-25 10:34:01 -0500

    adding more to the database

--- foods_table.sql
+++ foods_table.sql
@@ -55,6 +55,7 @@ INSERT INTO `foods` VALUES (49,'gerolsteiner_water_sparkling');
 INSERT INTO `foods` VALUES (50,'cabot_yogurt');
 INSERT INTO `foods` VALUES (51,'mountain_valley_water_sparkling');
 INSERT INTO `foods` VALUES (52,'ginger_root');
+INSERT INTO `foods` VALUES (53,'mandarin');
 CREATE INDEX IF NOT EXISTS `index_food_name` ON `foods` (
 	`food_name`
 );
